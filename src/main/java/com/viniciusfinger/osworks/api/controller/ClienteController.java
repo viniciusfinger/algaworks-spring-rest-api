39fb69d0b7559c7b5acc140e939789c89499d422
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-17 22:57:28 -0300

    Update

--- src/main/java/com/viniciusfinger/osworks/api/controller/ClienteController.java
+++ src/main/java/com/viniciusfinger/osworks/api/controller/ClienteController.java
@@ -17,12 +17,12 @@ public class ClienteController {
 		var cliente2 = new Cliente();
 		cliente1.setId(1l);
 		cliente1.setNome("Vinicius");
-		cliente1.setTelefone("51983043447");
-		cliente1.setEmail("vinicius.finger2018@gmail.com");
+		cliente1.setTelefone("5130321203");
+		cliente1.setEmail("teste@gmail.com");
 		
 		cliente2.setId(2l);
 		cliente2.setNome("Caue");
-		cliente2.setTelefone("5198304344");
+		cliente2.setTelefone("51983409403");
 		cliente2.setEmail("vinicius@gmail.com");
 		
 		return Arrays.asList(cliente1, cliente2);
