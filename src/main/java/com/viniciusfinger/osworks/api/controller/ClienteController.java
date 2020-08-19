33f92f943d381a8caa4a9101c4b1071dbf1837b1
Vinicius Finger <51798742+viniciusfinger@users.noreply.github.com>
2020-08-19 18:33:06 -0300

    Dia 2
    Adicao de persistenica e conexao ao banco de dados.

--- src/main/java/com/viniciusfinger/osworks/api/controller/ClienteController.java
+++ src/main/java/com/viniciusfinger/osworks/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.viniciusfinger.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,12 @@ import com.viniciusfinger.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		var cliente2 = new Cliente();
-		cliente1.setId(1l);
-		cliente1.setNome("Vinicius");
-		cliente1.setTelefone("51983043447");
-		cliente1.setEmail("vinicius.finger2018@gmail.com");
-		
-		cliente2.setId(2l);
-		cliente2.setNome("Caue");
-		cliente2.setTelefone("5198304344");
-		cliente2.setEmail("vinicius@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 		
 	}
 	
--- src/main/java/com/viniciusfinger/osworks/domain/model/Cliente.java
+++ src/main/java/com/viniciusfinger/osworks/domain/model/Cliente.java
@@ -1,12 +1,25 @@
 package com.viniciusfinger.osworks.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
 	private String nome;
 	private String email;
+	
+	@Column(name="fone")
 	private String telefone;
 	
-	public long getId() {
+	public long getId() { 
 		return id;
 	}
 	public void setId(long id) {
@@ -31,5 +44,25 @@ public class Cliente {
 		this.telefone = telefone;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 }
